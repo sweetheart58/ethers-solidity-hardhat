eeef4ef623453485b3698f640721b35bc745ada9
sweetheart <sweetheart199558@gmail.com>
2022-11-16 11:19:37 +0500

    setup deploy wallet file

---
+++ deploy/01-deploy-wallet.ts
@@ -0,0 +1,6 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deployWallet: DeployFunction = async (
+    hre: HardhatRuntimeEnvironment
+) => {};
