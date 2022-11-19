cf706ed669dded6d5cb43f4abde58a68d258a7bf
sweetheart <sweetheart199558@gmail.com>
2022-11-19 14:49:42 +0500

    write deposit script

--- scripts/deposit.ts
+++ scripts/deposit.ts
@@ -1,4 +1,26 @@
-const depositFunction: () => Promise<void> = async () => {};
+import { BigNumber, ContractTransaction } from "ethers";
+import { deployments, ethers, getNamedAccounts, network } from "hardhat";
+import { Wallet } from "../typechain-types";
+
+const depositFunction: () => Promise<void> = async () => {
+    const { deployer } = await getNamedAccounts();
+    const oneEther: BigNumber = ethers.utils.parseEther("1");
+
+    if (network.name == "hardhat") {
+        console.log("Running Script on Hardhat Network");
+        await deployments.fixture(["all"]);
+    } else {
+        console.log(`Running Script on ${network.name} Network`);
+    }
+
+    const wallet: Wallet = await ethers.getContract("Wallet", deployer);
+    console.log(`Wallet deployed at: ${wallet.address}`);
+
+    const tx: ContractTransaction = await wallet.deposit({ value: oneEther });
+    await tx.wait(1);
+
+    console.log("Funds Deposited!!!");
+};
 
 depositFunction()
     .then(() => process.exit(0))
