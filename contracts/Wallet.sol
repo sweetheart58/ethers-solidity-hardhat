47a7494ea004986ee7db4444110627dc11898302
sweetheart <sweetheart199558@gmail.com>
2022-12-01 19:33:15 +0500

    update to latest version

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.16;
 
 // * imports
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.7",
+    solidity: "0.8.16",
     networks: {
         hardhat: {
             chainId: 31337,
